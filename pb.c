1f4ea3b5e0bbb5ed03f2e0a6cbc6b6d3be460861
Harsh Garg <gargharsh2512@gmail.com>
2022-12-11 16:38:50 +0530

    Add entries, display all entries, search for entries

--- pb.c
+++ pb.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Declaring a structure to store the address 
 add -> address
@@ -58,6 +59,129 @@ struct pb{
     struct add address;
 };
 
+void printTableHeader(){
+    printf("+");
+    for ( int i = 0; i < 7; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 15; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 15; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 6; i ++ ){
+        printf("-");
+    }
+    printf("+\n");
+    
+    printf("|S.No.\r\t|Name\r\t\t\t|Phone Number\r\t\t\t\t\t|Gender|");
+
+    printf("\n+");
+    for ( int i = 0; i < 7; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 15; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 15; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 6; i ++ ){
+        printf("-");
+    }
+    printf("+");
+}
+
+void printTableHeaderAll(){
+    printf("+");
+    for ( int i = 0; i < 7; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 15; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 15; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 7; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 31; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 15; i ++ ){
+        printf("-");
+    }
+    printf("+\n");
+    
+    printf("|S.No.\r\t|Name\r\t\t\t|Phone Number\r\t\t\t\t\t|Gender\r\t\t\t\t\t\t|Email Address\r\t\t\t\t\t\t\t\t\t\t|Alter. Phone\r\t\t\t\t\t\t\t\t\t\t\t\t|");
+
+    printf("\n+");
+    for ( int i = 0; i < 7; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 15; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 15; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 7; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 31; i ++ ){
+        printf("-");
+    }
+    printf("+");
+    for ( int i = 0; i < 15; i ++ ){
+        printf("-");
+    }
+    printf("+");
+}
+
+void printTableFooterAll(){
+    printf("\n+");
+            for ( int i = 0; i < 7; i ++ ){
+                printf("-");
+            }
+            printf("+");
+            for ( int i = 0; i < 15; i ++ ){
+                printf("-");
+            }
+            printf("+");
+            for ( int i = 0; i < 15; i ++ ){
+                printf("-");
+            }
+            printf("+");
+            for ( int i = 0; i < 7; i ++ ){
+                printf("-");
+            }
+            printf("+");
+            for ( int i = 0; i < 31; i ++ ){
+                printf("-");
+            }
+            printf("+");
+            for ( int i = 0; i < 15; i ++ ){
+                printf("-");
+            }
+            printf("+");
+}
 
 /* Clearing the output screen */
 void cls(){
@@ -68,6 +192,184 @@ void cls(){
     /* If the user's OS is Windows */
         system("cls");
     #endif
+}
+
+void strToCap(char ar[]){
+    if ( ar[0] >= 'a' && ar[0] <= 'z' ){
+        ar[0] = ar[0] - 32;
+    }
+
+    for ( int i = 1; ar[i] != '\0'; i++ ){
+        if ( ar[i] >= 'A' && ar[i] <= 'Z') {
+            ar[i] = ar[i] + 32;
+        }
+    }
+}
+
+
+/* Sorting the elements of the phonebook in incresasing order of their lastname, firstname */
+
+// TODO make it work
+void sortdDB(){
+
+    
+    /* Using bubble sort to sort the data */
+    int ctr = -1;
+    while ( ctr ){
+        
+        /* Opening the file */
+        FILE *fp;
+        FILE *fp_temp;
+
+        fp = fopen("pbbin.bin", "wb+");
+        fp_temp = fopen("pbbintem.bin", "wb+");
+
+        /* Some error in opening the file */
+        if ( fp == NULL || fp_temp == NULL ){
+            printf("Some Error Occurred");
+            exit(1);
+        }
+
+        ctr = 0;
+        int ptr = 0;
+
+        while ( !feof(fp) ){
+            struct pb el1;
+            struct pb el2;
+            fseek(fp, sizeof(struct pb)*ptr, SEEK_SET);
+
+            fread(&el1, sizeof(struct pb), 1, fp);
+            fread(&el2, sizeof(struct pb), 1, fp);
+            
+            if ( strcmp(el1.uname.lname, el2.uname.lname) < 0 ){
+                fwrite(&el2, sizeof(struct pb), 1, fp_temp);
+                fwrite(&el1, sizeof(struct pb), 1, fp_temp);
+                ctr++;
+            }else if ( strcmp(el1.uname.lname, el2.uname.lname) == 0 ){
+                if ( strcmp(el1.uname.fname, el2.uname.fname) < 0 ){
+                    fwrite(&el2, sizeof(struct pb), 1, fp_temp);
+                    fwrite(&el1, sizeof(struct pb), 1, fp_temp);
+                    ctr++;
+                }else {
+                    fwrite(&el1, sizeof(struct pb), 1, fp_temp);
+                    fwrite(&el2, sizeof(struct pb), 1, fp_temp);
+                }
+            }else {
+                fwrite(&el1, sizeof(struct pb), 1, fp_temp);
+                fwrite(&el2, sizeof(struct pb), 1, fp_temp);
+            }
+            ptr++;
+        }
+        
+        fclose(fp);
+        fclose(fp_temp);
+        printf("lfdhasjkdfhlasdjf");
+
+        remove("pbbin.bin");
+        rename("pbbintem.bin", "pbbin.bins");
+    }
+
+}
+
+
+/* Searches and displays all the records according to user input criteria
+crit -> criteria -> first name, last name or phone number
+term -> the word/number to search for
+ */
+
+void searchDisplay(char crit, char term[]){
+    FILE *fp;
+    fp = fopen("pbbin.bin", "rb+");
+    fseek(fp, 0, SEEK_SET); 
+    if ( fp == NULL ){
+        printf("Error occurred!\n");
+        return;
+    }
+
+    struct pb el;
+    int ctr = 1; 
+    int flag = 0; // To print the header banner if record found
+    /* Searching on the basis of first name */
+    if ( crit == 'f' ){
+        while ( 1 ){
+            if ( feof( fp ) ){
+                break;
+            }
+            strToCap(term);
+            fread(&el, sizeof(struct pb), 1, fp);
+            if ( strcmp(el.uname.fname, term) == 0 ){
+                if ( flag == 0 ){
+                    printTableHeaderAll();
+                    flag++;
+                }
+
+                printf("\n|%d\r\t|%s %s\r\t\t\t|%s\r\t\t\t\t\t|%c\r\t\t\t\t\t\t|%s\r\t\t\t\t\t\t\t\t\t\t|%s\r\t\t\t\t\t\t\t\t\t\t\t\t|", ctr, el.uname.fname, el.uname.lname, el.pphone, el.gender, el.email, el.aphone);
+            }
+            ctr++;
+            fread(&el, sizeof(struct pb), 1, fp );
+            if ( feof( fp ) ){
+                break;
+            }else{
+                fseek(fp, -(sizeof(struct pb)), SEEK_CUR);
+            }
+        }
+        if ( flag != 0 ) {
+           printTableFooterAll();
+        }
+    }
+    if ( crit == 'l' ){
+        while ( 1 ){
+            
+            strToCap(term);
+            fread(&el, sizeof(struct pb), 1, fp);
+            if ( strcmp(el.uname.lname, term) == 0 ){
+                if ( flag == 0 ){
+                    printTableHeaderAll();
+                    flag++;
+                }
+
+                printf("\n|%d\r\t|%s, %s\r\t\t\t|%s\r\t\t\t\t\t|%c\r\t\t\t\t\t\t|%s\r\t\t\t\t\t\t\t\t\t\t|%s\r\t\t\t\t\t\t\t\t\t\t\t\t|", ctr, el.uname.lname, el.uname.fname, el.pphone, el.gender, el.email, el.aphone);
+            }
+            ctr++;
+            fread(&el, sizeof(struct pb), 1, fp );
+            if ( feof( fp ) ){
+                break;
+            }else{
+                fseek(fp, -(sizeof(struct pb)), SEEK_CUR);
+            }
+        }
+        if ( flag != 0) {
+           printTableFooterAll();
+        }
+    }
+    if ( crit == 'p' ){
+        while ( 1 ){
+
+            fread(&el, sizeof(struct pb), 1, fp);
+            if ( strcmp(el.pphone, term) == 0 || strcmp(el.aphone, term) == 0 ){
+                if ( flag == 0 ){
+                    printTableHeaderAll();
+                    flag++;
+                }
+
+                printf("\n|%d\r\t|%s, %s\r\t\t\t|%s\r\t\t\t\t\t|%c\r\t\t\t\t\t\t|%s\r\t\t\t\t\t\t\t\t\t\t|%s\r\t\t\t\t\t\t\t\t\t\t\t\t|", ctr, el.uname.lname, el.uname.fname, el.pphone, el.gender, el.email, el.aphone);
+            }
+            ctr++;
+            fread(&el, sizeof(struct pb), 1, fp );
+            if ( feof( fp ) ){
+                break;
+            }else{
+                fseek(fp, -(sizeof(struct pb)), SEEK_CUR);
+            }
+        }
+        if ( flag != 0 ){
+           printTableFooterAll(); 
+        }
+    }
+
+    if ( flag == 0 ){
+        printf("\nNo records found with the search criteria!\n");
+    }
 }
 
 void main(){
@@ -90,8 +392,11 @@ void main(){
     /* Printing the menu in a loop */
     while ( 1 ){
         int opt;
-        printf("Enter the option:\n0. Exit\n1. Enter record\n2. Show all records\n: ");
+        printf("Enter the option:\n0. Exit\n1. Enter record\n2. Show all records\n3. Search\n: ");
         scanf("%d", &opt);
+        fflush(stdin);
+        
+        /* If the user wants to exit the program */
         if ( opt == 0 ){
             
             /* Printing the goodbye banner */
@@ -112,47 +417,49 @@ void main(){
             
             break;
         }
+
+        /* User does not want to exit */
         FILE *fp;
         
         switch ( opt ){
             case 1:
+                fflush(stdin);
                 /* Entering new record */
                 // Creating new phonebook element
                 struct pb ele;
+                printf("\n");
 
                 // Asking for the first name       
                 printf("Enter first name: ");
                 fflush(stdin);
-                gets(ele.uname.fname);
-                printf("\n");
+                scanf(" %s", ele.uname.fname);
+                // strncat(ele.uname.fname, '\0');
+                strToCap(ele.uname.fname);
 
                 // Asking for the surname
                 printf("Enter surname: ");
-                fflush(stdin);
-                gets(ele.uname.lname);
-                printf("\n");
+                scanf(" %s", ele.uname.lname);
+                // strncat(ele.uname.lname, '\0');
+                strToCap(ele.uname.lname);
 
                 // Asking for the personal phone number
                 printf("Enter phone number: +91 ");
-                fflush(stdin);
-                gets(ele.pphone);
-                printf("\n");
+                scanf(" %s", ele.pphone);
+                // strncat(ele.pphone, '\0');
 
                 // Asking for the alternate phone number
                 printf("Enter alternate phone number: ");
-                fflush(stdin);
-                gets(ele.aphone);
-                printf("\n");
+                scanf(" %s", ele.aphone);
+                // strncat(ele.aphone, '\0');
 
                 // Asking for the gender
                 printf("Enter gender (M/F/T): ");
                 char gen;
-                fflush(stdin);
-                scanf("%c", &gen);
+                scanf(" %c", &gen);
 
                 /* Converting the gender to upper case if it is in lower case for consistency */
-                if ( gen > 'a' && gen < 'z'){
-                    ele.gender = gen + 32;
+                if ( gen >= 'a' && gen <= 'z'){
+                    ele.gender = gen - 32;
                 }else{
                     ele.gender = gen;
                 }
@@ -162,37 +469,36 @@ void main(){
 
                 // Father's first name
                 printf("  First name: ");
-                fflush(stdin);
-                gets(ele.faname.fname);
-                printf("\n");
+                scanf(" %s", ele.faname.fname);
+                // strncat(ele.faname.fname, '\0');
+                strToCap(ele.faname.fname);
 
                 // Father's Last name
                 printf("  Last Name: ");
-                fflush(stdin);
-                gets(ele.faname.lname);
-                printf("\n");
+                scanf(" %s", ele.faname.lname);
+                // strncat(ele.faname.lname, '\0');
+                strToCap(ele.faname.lname);
 
                 /* Asking Mother's name */
                 printf("Enter Mother's name:\n");
 
                 // Mother's first name
                 printf("  First name: ");
-                fflush(stdin);
-                gets(ele.moname.fname);
-                printf("\n");
+                scanf(" %s", ele.moname.fname);
+                // strncat(ele.moname.fname, '\0');
+                strToCap(ele.moname.fname);
 
                 // Mother's Last name
                 printf("  Last Name: ");
-                fflush(stdin);
-                gets(ele.moname.lname);
-                printf("\n");
+                scanf(" %s", ele.moname.lname);
+                // strncat(ele.moname.lname, '\0');
+                strToCap(ele.moname.lname);
 
                 // TODO: Add regex
                 // Asking for the email id
                 printf("Enter you email id: ");
-                fflush(stdin);
-                gets(ele.email);
-                printf("\n");
+                scanf(" %s", ele.email);
+                // strncat(ele.email, '\0');
 
 
                 /* Asking for address */
@@ -200,39 +506,36 @@ void main(){
 
                 // Flat number
                 printf("  Flat number: ");
-                fflush(stdin);
-                gets(ele.address.fn);
-                printf("\n");
+                scanf(" %s", ele.address.fn);
+                // strncat(ele.address.fn, '\0');
 
                 // Building number
                 printf("  Building number: ");
-                fflush(stdin);
-                gets(ele.address.buil);
-                printf("\n");
+                scanf(" %s", ele.address.buil);
+                // strncat(ele.address.buil, '\0');
 
                 // Sreet Name
                 printf("  Street name: ");
-                fflush(stdin);
-                gets(ele.address.street);
-                printf("\n");
+                scanf(" %s", ele.address.street);
+                // strncat(ele.address.street, '\0');
+                strToCap(ele.address.street);
 
                 // City name
                 printf("  City: ");
-                fflush(stdin);
-                gets(ele.address.city);
-                printf("\n");
+                scanf(" %s", ele.address.city);
+                // strncat(ele.address.city, '\0');
+                strToCap(ele.address.city);
 
                 // State
                 printf("  State: ");
-                fflush(stdin);
-                gets(ele.address.state);
-                printf("\n");
+                scanf(" %s", ele.address.state);
+                // strncat(ele.uname.fname, '\0');
+                strToCap(ele.address.state);
 
                 // Pincode
                 printf("  Pincode: ");
-                fflush(stdin);
-                gets(ele.address.pin);
-                printf("\n");
+                scanf(" %s", ele.address.pin);
+                // strncat(ele.address.pin, '\0');
 
 
                 // Storing the data in a binary file
@@ -240,7 +543,7 @@ void main(){
                 fp = fopen("pbbin.bin", "ab+");
                 
                 // If there is an error in opening the file
-                if ( fp = NULL ){
+                if ( fp == NULL ){
                     printf("Some Error Occurred");
                     exit(1);
                 }
@@ -248,18 +551,18 @@ void main(){
                 fclose(fp);
                 
                 // Printing the success message
-                printf("Record saved.");
+                printf("Record saved.\n\n");
+                // TODO sortdDB();
                 break;
 
             case 2:
                 /* Displaying the basic details of all the entries */
-                printf("Jere");
-                fp = fopen("ppbin.bin", "rb");
-                printf("\nHere");
+                fp = fopen("pbbin.bin", "rb+");
+                printf("\n");
                 // If there is an error in opening the file
-                if ( fp = NULL ){
-                    printf("Some Error Occurred");
-                    exit(1);
+                if ( fp == NULL ){
+                    printf("No records found!\n\n");
+                    break;
                 }
                 int ctr = 0;
                 while( 1 ){
@@ -268,19 +571,66 @@ void main(){
                     
                     // If file is empty
                     if ( feof(fp) ){
-                        printf("No records found!\n");
                         break;
                     }
 
                     // Printing the headers
                     if ( ctr == 0 ){
-                        printf("S.No.\r\tName\r\t\t\tPhone Number\r\t\t\t\t\tGender");
-                        ctr++;
+                        printTableHeader();
                     }
+                    ctr++;
 
-                    printf("%d\r\t%s, %s\r\t\t\t%s\r\t\t\t\t\t%c", ctr, ele.uname.lname, ele.uname.fname, ele.pphone, ele.gender);
-                    printf("\n");
+                    printf("\n|%d\r\t|%s, %s\r\t\t\t|%s\r\t\t\t\t\t|%c     |", ctr, ele.uname.lname, ele.uname.fname, ele.pphone, ele.gender);
                 }
+
+                printf("\n+");
+                for ( int i = 0; i < 7; i ++ ){
+                    printf("-");
+                }
+                printf("+");
+                for ( int i = 0; i < 15; i ++ ){
+                    printf("-");
+                }
+                printf("+");
+                for ( int i = 0; i < 15; i ++ ){
+                    printf("-");
+                }
+                printf("+");
+                for ( int i = 0; i < 6; i ++ ){
+                    printf("-");
+                }
+                printf("+");
+                printf("\n\n");
+                break;
+            case 3:
+                printf("Select search criteria: \n  1. First Name\n  2. Last Name\n  3. Phone Number\n:");
+                int opt;
+                scanf("%d", &opt);
+
+                switch ( opt ){
+                    case 1:
+                        char fname[20];
+                        printf("Enter the first name you want to search: ");
+                        scanf(" %s", fname);
+                        searchDisplay('f', fname);
+                        break;
+                    case 2:
+                        char lname[20];
+                        printf("Enter the last name you want to search: ");
+                        scanf(" %s", lname);
+                        searchDisplay('l', lname);
+                        break;
+                    case 3:
+                        char ph[11];
+                        printf("Enter the phone number you want to search: ");
+                        fflush(stdin);
+                        scanf(" %s", ph);
+                        ph[10] = '\0';
+                        searchDisplay('p', ph);
+                        break;
+                }
+                printf("\n\n");
+                break;
         }
     }
 }
